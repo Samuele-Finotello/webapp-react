8e9e67490d959dd3ca5a2720484b5608083be6ec
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-21 15:12:56 +0200

    [add] feat create movies list in Homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,13 @@
 const Header = () => {
   return (
     <header>
-      <div className="ms-4 h-200">
-        <img id="logo" src="../logo.png" alt="Logo" />
+      <div className="ms-4 d-flex align-items-center">
+        <div className="h-200">
+          <img id="logo" src="../logo.png" alt="Logo" />
+        </div>
+        <div>
+          <p className="m-0 mt-3 fs-3"><i className="text-white">Dove il cinema prende vita!</i></p>
+        </div>
       </div>
     </header>
   )
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,39 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className='container'>
+      <div className="row">
+        <div className="col-12 text-center mt-4">
+          <h1>Lista film</h1>
+        </div>
+      </div>
+      <div className="row g-3">
+        <div className='col-4'>
+          <div className="card">
+            <img src="../logo.png" alt="" />
+          </div>
+        </div>
+        <div className='col-4'>
+          <div className="card">
+            <img src="../logo.png" alt="" />
+          </div>
+        </div>
+        <div className='col-4'>
+          <div className="card">
+            <img src="../logo.png" alt="" />
+          </div>
+        </div>
+        <div className='col-4'>
+          <div className="card">
+            <img src="../logo.png" alt="" />
+          </div>
+        </div>
+        <div className='col-4'>
+          <div className="card">
+            <img src="../logo.png" alt="" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
