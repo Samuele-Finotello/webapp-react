8b388c8e05a17233c8191b79c919cbbf97d0ec31
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-21 18:14:25 +0200

    [add] feat method .map for cards

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -21,51 +21,30 @@ const Homepage = () => {
         </div>
       </div>
       <div className="row g-3 my-5">
+        {movies.map(movie => {
+          return (
+            <div className='col-lg-4 col-6' key={movie.id}>
+              <div className="card">
+                <img src={movie.image} alt={movie.title} />
+                <div className='overlay text-center'>
+                  <h3>{movie.title}</h3>
+                  <p>{movie.director}</p>
+                  <p>{movie.abstract}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )
+        })}
         <div className='col-lg-4 col-6'>
           <div className="card">
             <img src="../inception.jpg" alt="" />
             <div className='overlay text-center'>
               <h3>Nome film</h3>
               <p>Regista</p>
+              <p>Descrizione</p>
             </div>
           </div>
-        </div>
-        <div className='col-lg-4 col-6'>
-          <div className="card">
-            <img src="../interstellar.jpg" alt="" />
-            <div className='overlay text-center'>
-              <h3>Nome film</h3>
-              <p>Regista</p>
-            </div>
-          </div>
-        </div>
-        <div className='col-lg-4 col-6'>
-          <div className="card">
-            <img src="../matrix.jpg" alt="" />
-            <div className='overlay text-center'>
-              <h3>Nome film</h3>
-              <p>Regista</p>
-            </div>
-          </div>
-        </div>
-        <div className='col-lg-4 col-6'>
-          <div className="card">
-            <img src="../the_godfather.jpg" alt="" />
-            <div className='overlay text-center'>
-              <h3>Nome film</h3>
-              <p>Regista</p>
-            </div>
-          </div>
-        </div>
-        <div className='col-lg-4 col-6'>
-          <div className="card">
-            <img src="../titanic.jpg" alt="" />
-            <div className='overlay text-center'>
-              <h3>Nome film</h3>
-              <p>Regista</p>
-            </div>
-          </div>
-
         </div>
       </div>
     </div>
