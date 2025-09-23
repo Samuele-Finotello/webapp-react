9d9dbaedd2e755f7eca34af7eb53e2a9219bab72
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-23 22:29:06 +0200

    [add] feat create Link

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
-
+import { Link } from "react-router-dom"
 
 const MovieCard = ({ movie }) => {
   return (
     <div className='col-sm-12 col-md-6 col-xl-4' key={movie.id}>
-      <div className="card">
-        <img src={movie.image} alt={movie.title} />
-        <div className='overlay text-center'>
-          <h2>{movie.title}</h2>
-          <h4>{movie.director}</h4>
+      <Link to={`/movies/${movie.id}`}>
+        <div className="card">
+          <img src={movie.image} alt={movie.title} />
+          <div className='overlay text-center'>
+            <h2>{movie.title}</h2>
+            <h4>{movie.director}</h4>
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
