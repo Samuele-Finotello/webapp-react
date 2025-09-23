32d34ac219b7c30001e73a76568a458b97ef8a1f
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-09-23 23:41:37 +0200

    [add] feat create button back to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,21 @@
-
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
     <header>
-      <div className="ms-4 d-flex align-items-center">
-        <div className="h-200">
-          <img id="logo" src="../logo.png" alt="Logo" />
+      <div className="ms-4 d-flex justify-content-between align-items-center me-5">
+        <div className="d-flex align-items-center">
+          <div className="h-200">
+            <img id="logo" src="../logo.png" alt="Logo" />
+          </div>
+          <div>
+            <p className="m-0 mt-3 fs-3"><i className="text-white">Dove il cinema prende vita!</i></p>
+          </div>
         </div>
         <div>
-          <p className="m-0 mt-3 fs-3"><i className="text-white">Dove il cinema prende vita!</i></p>
+          <Link className="btn btn-primary fs-4 " to={'/'}>
+            Torna alla homepage
+          </Link>
         </div>
       </div>
     </header>
